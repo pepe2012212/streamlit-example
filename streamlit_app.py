26bd7782cf03df1b39e3e78421d8f5433dc452bf
pepe2012212 <122749465+pepe2012212@users.noreply.github.com>
2023-06-27 19:09:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,55 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+from flask import Flask, render_template, request
 
-"""
-# Welcome to Streamlit!
+app = Flask(__name__)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+allQuestions = [
+    "What is your favorite memory of dating me?",
+    "What is your favorite memory of us?",
+    "What food reminds you of me?",
+    "When was the last time you thought about me in a positive way?",
+    "What is your favorite thing that I do for you?",
+    "What movie reminds you of us?",
+    # ... (add the remaining questions here)
+    "Do you ever wish I could read your mind? When?",
+    "What things about me make you know I’m the one for you?",
+    # Additional questions added
+    "What is your favorite memory of dating me?",
+    "What is your favorite memory of us?",
+    "What food reminds you of me?",
+    "When was the last time you thought about me in a positive way?",
+    "What is your favorite thing that I do for you?",
+    "What movie reminds you of us?",
+    "Which of your parents are you most like?",
+    "Which of our kids are most like you? (or if you aren’t parents yet: Do you ever picture having kids?)",
+    "What’s my best physical feature?",
+    "What do you like most that I do in bed?",
+    # ... (add the remaining questions here)
+    "Do you think I’m more of an optimist, a pessimist, or a realist?",
+    "As a teenager, did you ever rebel against your parents?",
+    "Who’s the closest person to you in your extended family?",
+    "Did you ever want more or fewer siblings?",
+    "How did your siblings shape who you are?",
+    "What was your favorite date night we ever had?",
+    "What are your secret thoughts when you see me at the end of the day?",
+    "Do you ever wish I could read your mind? When?",
+    "What things about me make you know I’m the one for you?"
+]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def generate_random_questions():
+    import random
+    selected_questions = random.sample(allQuestions, 10)
+    return selected_questions
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+@app.route('/')
+def index():
+    questions = generate_random_questions()
+    return render_template('index.html', questions=questions)
 
+@app.route('/submit', methods=['POST'])
+def submit_answers():
+    answers = request.form.getlist('answer')
+    # Process and store the answers as needed
+    return 'Answers submitted successfully!'
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == '__main__':
+    app.run()
